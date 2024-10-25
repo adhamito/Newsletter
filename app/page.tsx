3e04e0e830efb874a322942a0ccc9c5f293886a2
adhamito <aalmechkor2@gmail.com>
2024-10-25 18:16:51 +0100

    Update page content and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+import React, { useState } from "react";
+import SectionText from "./components/SectionText";
+import SectionImg from "./components/SectionImg";
+import SuccessText from "./components/SuccessText";
+
+// SuccessText Component
+
 export default function Home() {
+  const [success, setSuccess] = useState(false);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="shadow-md items-center ml-56">
+      <div className="p-16 text-black md:w-[850px] w-full">
+        <div className="bg-transparent flex md:flex-row md:justify-between flex-col justify-center items-center rounded-2xl shadow-md">
+          {success ? (
+            <div className="order-1 md:order-1 md:w-[450px] mx-auto items-start bg-white ">
+              <SuccessText />
+            </div>
+          ) : (
+            <>
+              <div className="bg-white flex md:flex-row">
+                <div className="order-1 md:order-1">
+                  <SectionText setSuccess={setSuccess} />
+                </div>
+                <div className="order-2 md:order-2">
+                  <SectionImg />
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
